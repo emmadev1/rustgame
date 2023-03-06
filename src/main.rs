9801d6445f7e1d3db8e40d92fb44e84ab6d44f73
Emma <99513625+emmadev1@users.noreply.github.com>
2023-03-06 18:49:53 +0100

    Separated player and ai into different structs and added enums for classes

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ enum Ctype {
 }
 
 #[derive(Debug)]
+enum Atype {
+    A,
+    B,
+    C,
+}
+
+#[derive(Debug)]
 enum Stype {
     A,
     B,
@@ -18,11 +25,11 @@ enum Stype {
 #[derive(Debug)]
 struct Stage {
     size: u8,
+    stype: Stype,
 }
 
 #[derive(Debug)]
 struct Player {
-    is_player: bool,
     name: String,
     ctype: Ctype,
     max_hp: i16,
@@ -32,10 +39,31 @@ struct Player {
     defense: i16,
 }
 
+#[derive(Debug)]
+struct Ai {
+    name: String,
+    ctype: Atype,
+    max_hp: i16,
+    current_hp: i16,
+    strength: i16,
+    magic: i16,
+    defense: i16,
+}
+
 impl Player {
-    fn attack_basic (&self, s: &Player) -> i16 {
+    fn attack_basic (&self, s: &Ai) -> i16 {
         self.strength - s.defense
     } 
+
+    fn attack_magic (&self, s: &Ai) -> i16 {
+        self.magic
+    }
+}
+
+impl Ai {
+    fn attack_basic (&self, s: &Player) -> i16 {
+        self.strength - s.defense
+    }
 
     fn attack_magic (&self, s: &Player) -> i16 {
         self.magic
@@ -63,18 +91,17 @@ fn game_init() {
     io::stdin().read_line(&mut player_name).expect("No");
     player_name = String::from(player_name.trim());
     
-    let mut player1 = player_init(true, player_name, Ctype::A);
+    let mut player1 = player_init(player_name, Ctype::A);
     println!("{:?}", player1);
 
-    let mut aiplayer1 = player_init(false, "Ai".to_string(), Ctype::B);
+    let mut aiplayer1 = ai_init("Ai".to_string(), Atype::B);
     println!("{:?}", aiplayer1);
 
     game_core(player1, aiplayer1);
 }
 
-fn player_init(f_is_player: bool, pname: String, class: Ctype) -> Player {
+fn player_init(pname: String, class: Ctype) -> Player {
     let mut player = Player {
-        is_player: f_is_player,
         name: pname,
         ctype: class,
         max_hp: thread_rng().gen_range(150..=200),
@@ -87,7 +114,21 @@ fn player_init(f_is_player: bool, pname: String, class: Ctype) -> Player {
     return player
 }
 
-fn game_core(mut p1: Player, mut ai1: Player) {
+fn ai_init(pname: String, class: Atype) -> Ai {
+    let mut ai = Ai {
+        name: pname,
+        ctype: class,
+        max_hp: thread_rng().gen_range(150..=200),
+        current_hp: 0,
+        strength: thread_rng().gen_range(20..=60),
+        magic: thread_rng().gen_range(30..=50),
+        defense: thread_rng().gen_range(0..=15),
+    };
+    ai.current_hp = ai.max_hp;
+    return ai
+}
+
+fn game_core(mut p1: Player, mut ai1: Ai) {
     loop {
         println!("Player \x1b[01;31mHP\x1b[0m: {}", p1.current_hp);
         println!("AI \x1b[01;31mHP\x1b[0m: {}", ai1.current_hp);
@@ -100,13 +141,13 @@ fn game_core(mut p1: Player, mut ai1: Player) {
         }
 
         ai1.current_hp -= usr_turn(&p1, &ai1);
-        p1.current_hp -= ai_turn(&p1, &ai1);
+        p1.current_hp -= ai_turn(&ai1, &p1);
 
         //thread::sleep(time::Duration::from_secs(2));
    }
 }
 
-fn usr_turn(p1: &Player, ai1: &Player) -> i16 {
+fn usr_turn(p1: &Player, ai1: &Ai) -> i16 {
     let mut usrin: String = String::new();
     println!("Choose a move (impl 1 and 2): ");
     io::stdin().read_line(&mut usrin).expect("no");
@@ -123,7 +164,7 @@ fn usr_turn(p1: &Player, ai1: &Player) -> i16 {
     }
 }
 
-fn ai_turn(ai1: &Player, p1: &Player) -> i16 {
+fn ai_turn(ai1: &Ai, p1: &Player) -> i16 {
     let selec: u8 = thread_rng().gen_range(0..=1);
 
     if p1.defense > 11 {
